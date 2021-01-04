a33deabeb1f5eb2a87e2981014bff150c41ef0fa
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-01-04 01:02:17 +0100

    rebuilt docs

---
+++ scripts/resize.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd assets/images
+
+rm -rf mid
+mkdir -p mid
+
+for i in *.jpg; do
+    [ -f "$i" ] || break
+    convert $i -resize 3000x2000 mid/$i
+    chmod 666 mid/$i
+    jpegoptim --size 300k mid/$i
+done
+
+rm -rf sm
+mkdir -p sm
+
+for i in *.jpg; do
+    [ -f "$i" ] || break
+    convert $i -resize 1200x800 sm/$i
+    chmod 666 mid/$i
+    jpegoptim --size 150k sm/$i
+done
